2096645773846e76afb8a1e2b3e368c711591f41
root <joe@codeherent.co.uk>
2021-02-25 13:58:14 +0000

    Thu Feb 25 2021 - Committed via Codeherent

--- azure/azure_linux_docker_app_service/monitoring.tf
+++ azure/azure_linux_docker_app_service/monitoring.tf
@@ -7,7 +7,6 @@ resource "azurerm_monitor_action_group" "current" {
   name                = "SendAlertEmail"
   resource_group_name = data.azurerm_resource_group.current.name
   short_name          = "Alert"
-
   email_receiver {
     name          = "sendtoemail"
     email_address = var.alert_email_address
@@ -24,8 +23,13 @@ resource "azurerm_application_insights_web_test" "app_availability" {
   frequency               = 300
   timeout                 = 60
   enabled                 = true
-  geo_locations           = ["emea-nl-ams-azr", "emea-ru-msa-edge", "emea-gb-db3-azr", "emea-fr-pra-edge", "us-va-ash-azr"]
-
+  geo_locations = [
+    "emea-nl-ams-azr",
+    "emea-ru-msa-edge",
+    "emea-gb-db3-azr",
+    "emea-fr-pra-edge",
+    "us-va-ash-azr",
+  ]
   configuration = <<XML
 <WebTest Name="Availability" Id="9a572603-75a7-4754-8f17-74d3a428d7fa" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="120" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
   <Items>
@@ -33,10 +37,9 @@ resource "azurerm_application_insights_web_test" "app_availability" {
   </Items>
 </WebTest>
 XML
-
   lifecycle {
     ignore_changes = [
-      tags
+      tags,
     ]
   }
 }
@@ -49,41 +52,35 @@ resource "azurerm_monitor_metric_alert" "app_availability" {
   # https://github.com/terraform-providers/terraform-provider-azurerm/issues/8551
   scopes = [
     azurerm_application_insights_web_test.app_availability.id,
-    azurerm_application_insights.current.id
+    azurerm_application_insights.current.id,
   ]
-
   # Every 1 mins in 5 min window
   frequency   = "PT1M"
   window_size = "PT5M"
   # Critical
   severity = 0
-
   application_insights_web_test_location_availability_criteria {
     web_test_id           = azurerm_application_insights_web_test.app_availability.id
     component_id          = azurerm_application_insights.current.id
     failed_location_count = 3
   }
-
   action {
     action_group_id = azurerm_monitor_action_group.current.id
   }
 }
-
 
 # HTTP 5xx errors
 resource "azurerm_monitor_metric_alert" "ms_5xx_errors" {
   name                = "${azurerm_app_service.current.name} server had HTTP 5xx errors"
   resource_group_name = data.azurerm_resource_group.current.name
   scopes = [
-    azurerm_app_service.current.id
+    azurerm_app_service.current.id,
   ]
-
   # Every 15 mins in 15 min window
   frequency   = "PT15M"
   window_size = "PT15M"
   # Error
   severity = 1
-
   criteria {
     metric_namespace = "Microsoft.Web/sites"
     metric_name      = "Http5xx"
@@ -91,7 +88,6 @@ resource "azurerm_monitor_metric_alert" "ms_5xx_errors" {
     operator         = "GreaterThan"
     threshold        = 0
   }
-
   action {
     action_group_id = azurerm_monitor_action_group.current.id
   }
@@ -111,15 +107,13 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "dependency_failures_in_a
   dependencies
   | where resultCode == "False"
 QUERY
-
   trigger {
     operator  = "GreaterThan"
     threshold = 0
   }
-
   action {
     action_group = [
-      azurerm_monitor_action_group.current.id
+      azurerm_monitor_action_group.current.id,
     ]
   }
 }
